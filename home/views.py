1d7391d6dc85c1c3e92414865d3218253f87ac56
babak <babak.esi1381@gmail.com>
2024-05-31 02:20:03 +0330

    search done (but need update)

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from .models import Product
+from .models import Product,Category
 from .serializers import ProductSerializer
-from rest_framework import filters 
-
+from rest_framework import filters as drf_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 class HomeView(ListAPIView):
     queryset = Product.objects.filter(available=True)
@@ -19,14 +21,22 @@ class HomeView(ListAPIView):
         # Return queryset excluding products with zero stock
         return Product.objects.filter(available=True).order_by('-created_at')
     
+class ProductFilter(filters.FilterSet):
+    category = filters.ModelChoiceFilter(queryset=Category.objects.all())
+    price = filters.NumberFilter()
+    size = filters.ChoiceFilter(choices=[(size, size) for size in Product.objects.values_list('size', flat=True).distinct()])
+    color = filters.ChoiceFilter(choices=[(color, color) for color in Product.objects.values_list('color', flat=True).distinct()])
+
+    class Meta:
+        model = Product
+        fields = ['category', 'price', 'size', 'color']
+
 class ProductSearchView(ListAPIView):
-    queryset =Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['$name']
-    
-
-
+    filter_backends = [drf_filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['name', 'category__title', 'price', 'size', 'color']
+    filterset_class = ProductFilter  # Use the filter set here
         
     
         
--- medesco/settings.py
+++ medesco/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'home',
     'accounts',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
